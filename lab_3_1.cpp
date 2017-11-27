560d00b5aecec245762c537316130259d59e09da
Unknown <sodomyoindy@gmail.com>
2017-11-27 18:09:31 +0700

    ทำโจทย์ข้อที่ 1
    โดยการใช้ cout <<""<<endl; เพื่อแสดงข้อความให้ขึ้นมา

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss.Yositar kongthaworn"<<endl;
+  cout <<"Nickname Yo ,Sex Female"<<endl;
+  cout <<"Birthdays 1998/12/19 "<<endl;
+  cout <<"Hobbies Read manga, watch cartoons and sleep." <<endl;
+  cout <<"Status single"<<endl;
+
   return 0;
 }
